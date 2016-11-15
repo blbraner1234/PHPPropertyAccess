43e23812844b78267c0d5db3dc03c9829d7984d5
Brandon Braner <brandon.braner@ave81.com>
2016-11-15 14:44:06 -0600

    added some doc blocks for PropertyAccess Trait

--- src/PropertyAccess/traits/PropertyAccess.php
+++ src/PropertyAccess/traits/PropertyAccess.php
@@ -8,6 +8,14 @@ use teleiosis\PropertyAccess\lib\ParseAnnotations;
 trait PropertyAccess
 {
 
+    /**
+     * Overwride __set method to pass to a method on current object that
+     * is annotated with @property and has the same name as property
+     * @param $name
+     * @param $value
+     *
+     * @return mixed
+     */
     public function __set($name, $value)
     {
         if(method_exists($this, $name) && $this->checkAnnotations($name, 'property')){
@@ -16,6 +24,14 @@ trait PropertyAccess
         return $this->$name = $value;
     }
 
+    /**
+     * * Overwride __get method to pass to a method on current object that
+     * is annotated with @property and has the same name as property
+     *
+     * @param $name
+     *
+     * @return mixed
+     */
     public function __get($name)
     {
         if(method_exists($this, $name) && $this->checkAnnotations($name, 'property')){
@@ -24,6 +40,13 @@ trait PropertyAccess
         return $this->$name;
     }
 
+    /**
+     * * Overwride __isset method to pass to a method on current object that
+     * is annotated with @property and has the same name as property
+     * @param $name
+     *
+     * @return bool
+     */
     public function __isset($name)
     {
         if(method_exists($this, $name) && $this->checkAnnotations($name, 'property')){
@@ -40,6 +63,14 @@ trait PropertyAccess
     }
 
 
+    /**
+     * Method to check the annotations for the method and property name on
+     * current called class
+     * @param $methodName
+     * @param $propertyName
+     *
+     * @return mixed
+     */
     public function checkAnnotations($methodName, $propertyName)
     {
         $annotation = new ParseAnnotations();
